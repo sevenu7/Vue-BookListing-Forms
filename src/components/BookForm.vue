743161b0bdaa00f9c19db78657b0944883e0695b
Sugguna <venu.sugguna@capgemini.com>
2021-01-04 15:55:55 +0530

    module 1

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,7 +1,17 @@
 <template>
-  <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)" class="" action="#" method="post">
-    <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
-    <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+  <form v-on:submit.prevent="bookSubmit(bookData)" class="" action="#" method="post">
+    <input v-model="bookData.bookTitle" type="text" name="title" value="" placeholder="Book Title">
+    <input v-model="bookData.bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+    <div>
+      <input type="checkbox" name="finishedReading" v-model="bookData.finishedReading">
+      <label for="finishedReading">Finished Reading</label>
+    </div>
+    <div>
+      <label for="borrowed">borrowed</label>
+      <input type="radio" value="borrowed" v-model="bookData.ownership">
+      <label for="bought">bought</label>
+      <input type="radio" value="bought" v-model="bookData.ownership">
+    </div>
     <button type="submit" name="button">Add Book</button>
   </form>
 </template>
@@ -12,13 +22,17 @@ export default {
   props: ["books"],
   data() {
     return {
-      bookTitle: "",
-      bookAuthor: ""
+      bookData: {
+        bookTitle: "",
+        bookAuthor: "",
+        finishedReading: false,
+        ownership: []
+      }
     };
   },
   methods: {
-    bookSubmit(bookTitle, bookAuthor) {
-      this.$emit("addBook", bookTitle, bookAuthor);
+    bookSubmit(bookData) {
+      this.$emit("addBook", bookData.bookTitle, bookData.bookAuthor);
     }
   }
 };
