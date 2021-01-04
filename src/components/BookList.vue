8a295d26926fc054c01aa6b5319dbd9eb0b282b9
Sugguna <venu.sugguna@capgemini.com>
2021-01-04 16:37:11 +0530

    module 3

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
-    <h1>{{title}}</h1>
+    <h1>{{ title }}</h1>
     <ul>
-      <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
+      <book-item v-for="book in books" :key="book.id" :book="book"></book-item>
     </ul>
-    <br><hr>
-    <book-form @addBook='appendBook'></book-form>
+    <hr />
+    <h2>Filtered Books By Ownership</h2>
+    <select v-model="holding">
+      <option v-for="filter in filters" :key="filter">{{ filter }}</option>
+    </select>
+    <ul>
+      <book-item v-for="book in filteredBooks" :key="book.id" :book="book"></book-item>
+    </ul>
+    <br />
+    <hr />
+    <book-form @addBook="appendBook"></book-form>
   </div>
 </template>
 
 <script>
+import _ from "lodash";
 import BookItem from "./BookItem";
 import BookForm from "./BookForm";
 
@@ -19,22 +29,50 @@ export default {
     return {
       title: "All Books",
       states: ["Want to Read", "Read", "Reading"],
+      filters: ["bought", "borrowed"],
+      holding: "bought",
       books: [
-        { title: "Self-Reliance", author: "Ralph Waldo Emerson", finishedReading: true },
-        { title: "American Gods", author: "Neil Gaiman", finishedReading: false },
-        { title: "Amusing Ourselves to Death", author: "Neil Postman", finishedReading: true }
-      ]
+        {
+          title: "Self-Reliance",
+          author: "Ralph Waldo Emerson",
+          finishedReading: true,
+          ownership: "borrowed",
+        },
+        {
+          title: "American Gods",
+          author: "Neil Gaiman",
+          finishedReading: false,
+          ownership: "bought",
+        },
+        {
+          title: "Amusing Ourselves to Death",
+          author: "Neil Postman",
+          finishedReading: true,
+          ownership: "borrowed",
+        },
+      ],
     };
   },
   components: {
     BookItem,
-    BookForm
+    BookForm,
+  },
+  computed: {
+    filteredBooks() {
+      return _.filter(this.books, ["ownership", this.holding]);
+      // return this.books.filter(book => book.ownership === this.holding);
+    }
   },
   methods: {
     appendBook(bookData) {
-      this.books.push({ title: bookData.bookTitle, author: bookData.bookAuthor, finishedReading: bookData.finishedReading });
-    }
-  }
+      this.books.push({
+        title: bookData.bookTitle,
+        author: bookData.bookAuthor,
+        finishedReading: bookData.finishedReading,
+        ownership: bookData.ownership,
+      });
+    },
+  },
 };
 </script>
 
